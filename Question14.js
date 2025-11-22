13078a4258b05e6bb31daab7ec39e52541cc2ff7
THUTAHEMALATHA <thutahemalatha@gmail.com>
2025-11-22 13:51:31 +0530

    Merging and Updating Nested Objects with Spread Operator

---
+++ Question14.js
@@ -0,0 +1,10 @@
+const profile = { name: "Charlie", age: 29, address: { city: "San Francisco", zipcode: "94101" } };
+const updates = { age: 30, address: { zipcode: "94109", country: "USA" } };
+
+const mergedObjectNested = {
+    ...profile,
+    ...updates,
+    address: { ...profile.address, ...updates.address }
+};
+
+console.log(mergedObjectNested);
